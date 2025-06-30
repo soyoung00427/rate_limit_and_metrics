aeb604abdee7b11a6bb7f2059760c382e27e69b7
yeonchohee <duschgml0228@gmail.com>
2025-06-30 23:00:09 +0900

    알고리즘 - 슬라이딩윈도우 기본기능 Test
    1. 요청 허용
    2. 요청 거절
    3. 오래된 요청 삭제 후 재요청

--- rate-limit-algorithms/src/test/java/com/icd/SlidingWindowRateLimiterTest.java
+++ rate-limit-algorithms/src/test/java/com/icd/SlidingWindowRateLimiterTest.java
@@ -7,14 +7,35 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SlidingWindowRateLimiterTest {
 
     @Test
-    public void testTryAcquire() throws InterruptedException {
-        SlidingWindowRateLimiter limiter = new SlidingWindowRateLimiter(2, 1000);
+    void shouldAllowRequestsWithinLimit() {
+        SlidingWindowRateLimiter limiter = new SlidingWindowRateLimiter(5, 1000); // 1초에 5번
 
+        for(int i = 0; i < 5; i++) {
+            assertTrue(limiter.tryAcquire(), "요청" + (i+1) + " 은 허용돼야 함");
+        }
+
+    }
+
+    @Test
+    void shouldNotAllowRequestsWithoutLimit() {
+        SlidingWindowRateLimiter limiter = new SlidingWindowRateLimiter(3, 1000);
         assertTrue(limiter.tryAcquire());
+        assertTrue(limiter.tryAcquire());
+        assertTrue(limiter.tryAcquire());
+
+        assertFalse(limiter.tryAcquire(), "4째 요청은 거절돼야함");
+    }
+
+    @Test
+    void shouldAllowNewRequestsAfterOldOnesExpire() throws InterruptedException {
+        SlidingWindowRateLimiter limiter = new SlidingWindowRateLimiter(2, 500);
+        assertTrue(limiter.tryAcquire());
+        assertTrue(limiter.tryAcquire());
+
         assertFalse(limiter.tryAcquire());
-
-        Thread.sleep(1100);
+        Thread.sleep(600);
 
         assertTrue(limiter.tryAcquire());
+
     }
 }

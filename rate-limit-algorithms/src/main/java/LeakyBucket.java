9bf15d6ccad20509209abd0b47ccd9364f9c1271
songsimo <chy.song89>
2025-07-02 07:34:55 +0900

    feat: LeakyBucket 구현

--- rate-limit-algorithms/src/main/java/LeakyBucket.java
+++ rate-limit-algorithms/src/main/java/LeakyBucket.java
@@ -1,6 +1,8 @@
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class LeakyBucket {
+    public static final int STANDARD_PER_MILLISECOND = 1_000;
+
     private final long capacity;
     private final long leakRate;
     private AtomicInteger currentBucketAmount;
@@ -14,11 +16,29 @@ public class LeakyBucket {
     }
 
     public synchronized boolean allow() {
+        leak();
+
         if(currentBucketAmount.get() < capacity) {
             currentBucketAmount.incrementAndGet();
             return true;
         }
 
         return false;
+    }
+
+    private void leak() {
+        final long now = System.currentTimeMillis();
+        final long elapsed = now - lastLeakTimestamp;
+
+        if(now > lastLeakTimestamp) {
+            // 경과 시간 동안 누수된 양 계산
+            int leakedAmount = (int) ((elapsed / STANDARD_PER_MILLISECOND) * leakRate);
+
+            // 현재 버킷 레벨에서 누수된 양을 뺍니다.
+            int remainingAmount = Math.max(0, currentBucketAmount.get() - leakedAmount);
+
+            currentBucketAmount.set(remainingAmount);
+            lastLeakTimestamp = now;
+        }
     }
 }
--- rate-limit-algorithms/src/test/java/LeakyBucketTest.java
+++ rate-limit-algorithms/src/test/java/LeakyBucketTest.java
@@ -2,24 +2,39 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LeakyBucketTest {
-    private final long MAX_CAPACITY = 5;
-    private final long TWO_PER_PROCESS = 2;
+    private final int MAX_CAPACITY = 5;
+    private final int TWO_PER_SECOND = 2;
 
     private LeakyBucket leakyBucket;
 
     @BeforeEach
     void setUp() {
-        leakyBucket = new LeakyBucket(MAX_CAPACITY, TWO_PER_PROCESS);
+        leakyBucket = new LeakyBucket(MAX_CAPACITY, TWO_PER_SECOND);
     }
 
     @Test
-    @DisplayName("버킷이 수용할 수 있는지 확인")
+    @DisplayName("버킷 수용 안이면 true 넘치면 false")
     void allow() {
         for(int i = 0; i < MAX_CAPACITY; i++) {
+            assertTrue(leakyBucket.allow());
+        }
+
+        assertFalse(leakyBucket.allow());
+    }
+
+    @Test
+    @DisplayName("버킷의 누수 처리양 확인")
+    void leak() throws InterruptedException {
+        for(int i = 0; i < MAX_CAPACITY; i++) {
+            assertTrue(leakyBucket.allow());
+        }
+
+        Thread.sleep(LeakyBucket.STANDARD_PER_MILLISECOND);
+
+        for(int i = 0; i < TWO_PER_SECOND; i++) {
             assertTrue(leakyBucket.allow());
         }
 

7ce6c29c57ebb6b69b23459b65ffe799fcf58052
yeonchohee <duschgml0228@gmail.com>
2025-07-03 21:40:30 +0900

    알고리즘 - 슬라이딩윈도우
    1. 단일서버
    InMemorySlidingWindowRateLimiter.java
    
    2. 분산서버
    DistributedSlidingWindowRateLimiter.java

--- rate-limit-algorithms/src/main/java/com/icd/DistributedSlidingWindowRateLimiter.java
+++ rate-limit-algorithms/src/main/java/com/icd/DistributedSlidingWindowRateLimiter.java
@@ -6,15 +6,13 @@ import com.icd.ratelimiter.storage.RateLimiterStorage;
 
 import java.time.Duration;
 import java.time.Instant;
-/*
- * 분산서버 환경 Sliding Window 알고리즘
- */
+
 public class DistributedSlidingWindowRateLimiter implements RateLimiter {
 
     private final int maxRequests;
     private final Duration windowSize;
     private final RateLimiterStorage storage;
-    private final String ruleKey;  // 고유 키 구분용 (ex: "sliding_window")
+    private final String ruleKey;
 
     public DistributedSlidingWindowRateLimiter(int maxRequests, Duration windowSize, RateLimiterStorage storage, String ruleKey) {
         this.maxRequests = maxRequests;
@@ -27,13 +25,11 @@ public class DistributedSlidingWindowRateLimiter implements RateLimiter {
     public boolean allow(RequestContext context) {
         String userKey = context.getKey();
         Instant now = context.getTimestamp() != null ? context.getTimestamp() : Instant.now();
-
-        // 윈도우의 시작 시점(현재시간 - windowSize)
         Instant windowStart = now.minus(windowSize);
 
-        // 저장소에서 요청 기록 증가 및 카운트 조회
         String composedKey = ruleKey + ":" + userKey;
 
+        // storage 구현체가 windowStart~now 구간의 요청만 집계해야 함!
         long count = storage.incrementAndGet(composedKey, windowStart, windowSize);
 
         return count <= maxRequests;
